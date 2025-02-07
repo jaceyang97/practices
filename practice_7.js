babfd8a060de47803d5077abe3c8dae4a5cf5352
Jace Yang <jaceyang8@gmail.com>
2025-02-07 23:54:24 +0800

    update: add 4 dots

--- practice_7.js
+++ practice_7.js
@@ -11,6 +11,7 @@ function setup() {
     createCanvas(400, 400);
     drawSquare();
     drawThinSquare();
+    drawFourDots();
 }
 
 function drawSquare() {
@@ -48,5 +49,39 @@ function drawThinSquare() {
     strokeWeight(0.02);
     noFill();
     rect(80, 80, 240, 240);
+    pop();
+    
+}
+
+function drawFourDots() {
+    push();
+    
+    // Draw the connecting lines first (so dots appear on top)
+    stroke(0);
+    strokeWeight(1);
+    // Line from dot 1 to 2
+    line(100, 100, 100, 180);
+    // Line from dot 2 to 3
+    line(100, 180, 200, 180);
+    // Line from dot 3 to 4
+    line(200, 180, 200, 280);
+    
+    // Draw the dots
+    fill(0);
+    noStroke();
+    let dotSize = 5;
+
+    // Dot 1: Near top-left corner
+    ellipse(100, 100, dotSize);
+    
+    // Dot 2: Below dot 1
+    ellipse(100, 180, dotSize);
+    
+    // Dot 3: Right of dot 2
+    ellipse(200, 180, dotSize);
+    
+    // Dot 4: Below dot 3
+    ellipse(200, 280, dotSize);
+    
     pop();
 }
