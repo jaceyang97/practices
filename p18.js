82c0f6435c7efe79c1335eb950a96f20b87b4bc9
Jace Yang <jaceyang8@gmail.com>
2025-02-13 23:28:02 +0800

    chore: rename works

---
+++ p18.js
@@ -0,0 +1,8 @@
+/**
+ * Generative Art exercise from Tim Holman's Speedrun talk, CSSConf Australia 2018
+ * Reference: https://www.youtube.com/watch?v=4Se0_w0ISYk
+ * Timer: 11:56
+ *
+ * Practice 18: Recursion
+ * Code by Jace Yang
+ */
